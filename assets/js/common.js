ef65c570b64724a44cce3903d4af50ec484c0fa4
chenyansen <huisen0705@live.com>
2019-01-10 20:48:12 +0800

    commit

---
+++ assets/js/common.js
@@ -0,0 +1,17 @@
+//数组排序方法
+function sortNumber(a, b) {
+    return a - b;
+}
+//数组排序方法（倒序）
+function sortNumberDesc(a, b) {
+    return b - a;
+}
+
+//数组对象排序方法
+function sortArrObjectsByKey(array, key) {
+    return array.sort(function(a, b) {
+        let x = a[key];
+        let y = b[key];
+        return (x < y) ? -1 : ((x > y) ? 1 : 0);
+    }) ;
+}
