240ad03f5028878a1ba7d95f44d61eef5f0dd45b
chenyansen <huisen0705@live.com>
2019-05-06 07:36:39 +0800

    全局API

---
+++ components/MenuItem.vue
@@ -0,0 +1,17 @@
+// Menuitem
+   <style lang="stylus" scoped>
+    .item {
+        margin: 10px 0;
+        padding: 0 10px;
+        border-radius: 4px;
+        list-style: none;
+        background: skyblue;
+        color: #fff;
+    }
+    </style>
+
+<template>
+  <li class="item">
+    <slot />
+  </li>
+</template>
